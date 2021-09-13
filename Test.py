1fb726faece311062797327faae26854cb76dc39
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2021-09-13 18:46:27 +0200

    first commit

---
+++ Test.py
@@ -0,0 +1,44 @@
+#city ='portsaid'
+#city1 ='alex'
+#city2 ='cairo'
+#cit4 ='siina'
+# lists
+cities = [
+    'portsaid',
+    'alex',
+    'cairo',
+    'siina'
+]
+# collections
+animals = {
+    'birds': 'parrot',
+    'fish': 'white whale',
+    'tigers': 'tiger',
+}
+print(cities[0])
+print(animals['fish'])
+# challenge
+# Nearest stars to Earth
+#star1 = 'Sol'
+#star2 = 'Alpha Centauri'
+#star3 = 'Barnard'
+#star4 = 'Wolf 359'
+stars = ['Sol', 'Alpha Centauri', 'Barnard', 'Wolf 359']
+print(stars[3])
+#African = 'Kilimanjaro'
+#Antarctic = 'Vinson'
+#Australian = 'Puncak Jaya'
+#Eurasian = 'Everest'
+#North_American = 'Denali'
+#Pacific = 'Mauna Kea'
+#South_American = 'Aconcagua'
+peaks = {
+    'African': 'Kilimanjaro',
+    'Antarctic': 'Vinson',
+    'Australian': 'Puncak Jaya',
+    'Eurasian': 'Everest',
+    'North_American': 'Denali',
+    'Pacific': 'Mauna Kea',
+    'South_American': 'Aconcagua',
+}
+print(peaks['Pacific'])
